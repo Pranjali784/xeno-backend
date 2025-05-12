767c23b087b2a65c680ab0ee78abe73ba29ac78f
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2025-05-12 14:24:11 +0530

    CustomerController.java

--- src/main/java/com/xeno/crm/backend/controller/CustomerController.java
+++ src/main/java/com/xeno/crm/backend/controller/CustomerController.java
@@ -4,30 +4,28 @@ import com.xeno.crm.backend.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
 @RestController
 @RequestMapping("/api/customers")
 public class CustomerController {
+
     @Autowired
     private CustomerService customerService;
-
 
     @GetMapping
     public List<Customer> getAll() {
         return customerService.getAllCustomers();
     }
 
-
     @GetMapping("/{id}")
     public Customer getById(@PathVariable Long id) {
         return customerService.getCustomerById(id);
     }
 
-
     @PostMapping
     public Customer add(@RequestBody Customer customer) {
         return customerService.addCustomer(customer);
     }
-
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
