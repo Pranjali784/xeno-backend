da6f95e897927d09b9eafd3e0ccfcedab252a282
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2025-05-12 14:21:33 +0530

    CustomerRepository.java

--- src/main/java/com/xeno/crm/backend/repository/CustomerRepository.java
+++ src/main/java/com/xeno/crm/backend/repository/CustomerRepository.java
@@ -1,8 +1,7 @@
 package com.xeno.crm.backend.repository;
-
 import com.xeno.crm.backend.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+  
 }
 
