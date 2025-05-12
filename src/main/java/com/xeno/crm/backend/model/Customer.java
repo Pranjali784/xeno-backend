5dd1b97bcff1f7d3ff75ec6a338f2e9b4150e118
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2025-05-12 14:20:19 +0530

    Customer.java

--- src/main/java/com/xeno/crm/backend/model/Customer.java
+++ src/main/java/com/xeno/crm/backend/model/Customer.java
@@ -1,62 +1,44 @@
 package com.xeno.crm.backend.model;
-
 import jakarta.persistence.*;
 import java.util.List;
-
 @Entity
 public class Customer {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String email;
     private String phone;
-
     @OneToMany(mappedBy = "customer")
     private List<CommunicationLog> logs;
-
-    // Getters and setters
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
     public List<CommunicationLog> getLogs() {
         return logs;
     }
-
     public void setLogs(List<CommunicationLog> logs) {
         this.logs = logs;
     }
 }
-
