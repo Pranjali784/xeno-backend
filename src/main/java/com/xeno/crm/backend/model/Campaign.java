46131c534ff48812ae621e4cc6929407a947b3b4
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2025-05-12 14:18:37 +0530

    Campaign.java

--- src/main/java/com/xeno/crm/backend/model/Campaign.java
+++ src/main/java/com/xeno/crm/backend/model/Campaign.java
@@ -3,16 +3,12 @@ import jakarta.persistence.*;
 import java.util.Date;
 @Entity
 public class Campaign {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String description;
     private Date startDate;
     private Date endDate;
-
-    // Getters and setters
 }
 
