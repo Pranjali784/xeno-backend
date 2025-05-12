ee7d41175873473d86853e2ca1023b910493f861
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2025-05-12 14:19:15 +0530

    CommunicationLog.java

--- src/main/java/com/xeno/crm/backend/model/CommunicationLog.java
+++ src/main/java/com/xeno/crm/backend/model/CommunicationLog.java
@@ -1,20 +1,13 @@
 package com.xeno.crm.backend.model;
-
 import jakarta.persistence.*;
-
 @Entity
 public class CommunicationLog {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String channel;
     private String message;
-
     @ManyToOne
     private Customer customer;
-
-    // Getters and setters
 }
 
