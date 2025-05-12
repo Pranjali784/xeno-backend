ec9a672049cbfc6de58b04b405fa4ebdd701ff2b
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2025-05-12 14:07:40 +0530

    OrderController.java

--- src/main/java/com/xeno/crm/backend/controller/OrderController.java
+++ src/main/java/com/xeno/crm/backend/controller/OrderController.java
@@ -1,12 +1,9 @@
 package com.xeno.crm.backend.controller;
-
 import com.xeno.crm.backend.model.Order;
 import com.xeno.crm.backend.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
@@ -14,24 +11,27 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+
     @GetMapping
     public List<Order> getAll() {
         return orderService.getAllOrders();
     }
+
 
     @GetMapping("/{id}")
     public Order getById(@PathVariable Long id) {
         return orderService.getOrderById(id);
     }
 
+
     @PostMapping
     public Order add(@RequestBody Order order) {
         return orderService.addOrder(order);
     }
+
 
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         orderService.deleteOrder(id);
     }
 }
-
