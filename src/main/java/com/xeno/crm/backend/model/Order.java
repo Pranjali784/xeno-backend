d8b8abbdd4592ae8e93c24491670c59b761d5f3b
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2025-05-12 14:21:05 +0530

    Order.java

--- src/main/java/com/xeno/crm/backend/model/Order.java
+++ src/main/java/com/xeno/crm/backend/model/Order.java
@@ -1,53 +1,38 @@
 package com.xeno.crm.backend.model;
-
 import jakarta.persistence.*;
-
 @Entity
 @Table(name = "orders")
 public class Order {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String product;
     private int quantity;
     private double price;
-
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
-
-    // Getters and setters
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getProduct() {
         return product;
     }
-
     public void setProduct(String product) {
         this.product = product;
     }
-
     public int getQuantity() {
         return quantity;
     }
-
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
@@ -55,9 +40,7 @@ public class Order {
     public Customer getCustomer() {
         return customer;
     }
-
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
 }
-
