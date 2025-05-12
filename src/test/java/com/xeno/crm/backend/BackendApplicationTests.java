f6ac9657dfbdb99c46111f1368fe3dbe49ed951b
Pranjali784 <114579659+Pranjali784@users.noreply.github.com>
2025-05-12 14:17:03 +0530

    BackendApplicationTests.java

--- src/test/java/com/xeno/crm/backend/BackendApplicationTests.java
+++ src/test/java/com/xeno/crm/backend/BackendApplicationTests.java
@@ -1,13 +1,10 @@
 package com.xeno.crm.backend;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class BackendApplicationTests {
-
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
